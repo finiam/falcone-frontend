6743887552bb4287f2b01ff5e112f39be01e1027
Paula Carneiro <paulacarneiro215@gmail.com>
2023-06-12 16:22:28 +0100

    Fixes (#9)
    * quote token <-> base token
    
    * change options endpoint url
    
    * allow decimal option size

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,12 @@
-const BASE_URL = "https://api.carmine-dev.eu/api/v1/testnet";
+const BASE_URL = "https://api.carmine.finance/api/v1/testnet";
 
 export async function getRawAvailableOptions() {
   const req = await fetch(`${BASE_URL}/live-options`, {
     next: { revalidate: 10 },
   });
+
+  if (!req.ok) return {};
+
   const raw = await req.json();
 
   return raw;
