02eacb0b56e9c8dc2227c0dffee51545670abd02
davelange <lange@finiam.com>
2023-06-13 12:01:15 +0100

    add mdx packages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withMDX = require("@next/mdx")();
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure pageExtensions to include md and mdx
+  mdxRs: true,
+};
+
+module.exports = withMDX(nextConfig);
