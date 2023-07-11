9262c1c2f40793dd3233c23cae29b8c4afbabe2d
Paula Carneiro <paulacarneiro215@gmail.com>
2023-07-11 17:22:16 +0100

    style examples

--- src/modules/Concepts/ConceptsLayout.tsx
+++ src/modules/Concepts/ConceptsLayout.tsx
@@ -8,7 +8,7 @@ export default function ConceptsLayout({
   children: ReactNode;
 }) {
   return (
-    <div className="flex gap-[106px]">
+    <div className="flex gap-[106px] mr-12">
       <aside className="hidden lg:block w-[234px]">{sidebar}</aside>
       <main className="max-w-full w-[941px] px-4 lg:px-0 pb-20">
         {children}
